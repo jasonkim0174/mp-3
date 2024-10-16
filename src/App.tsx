bd71f202f7e5c5fdc4c2529eff9af8207b8c6a1a
Jason Kim <jasonkim0174@gmail.com>
2024-10-16 17:17:22 -0400

    changes to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
@@ -9,27 +9,49 @@ import Project from './pages/Projects';
 import References from './pages/References';
 import Skills from './pages/Skills';
 
-
 function App() {
   return (
-    <Router>
+    <>
       <Header />
       <div className="layout-container">
         <Navigation />
         <div className="content-container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/education" element={<Education />} />
-            <Route path="/experiences" element={<Experiences />} />
-            <Route path="/project" element={<Project />} />
-            <Route path="/references" element={<References />} />
-            <Route path="/skills" element={<Skills />} />
-          </Routes>
+          <RouterProvider router={router} />
         </div>
       </div>
       <Footer />
-    </Router>
+    </>
   );
 }
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/education',
+    element: <Education />,
+  },
+  {
+    path: '/experiences',
+    element: <Experiences />,
+  },
+  {
+    path: '/project',
+    element: <Project />,
+  },
+  {
+    path: '/references',
+    element: <References />,
+  },
+  {
+    path: '/skills',
+    element: <Skills />,
+  },
+]);
+
 
 export default App;
